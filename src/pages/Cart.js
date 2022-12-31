43b7904508ed60c78d8ae2b5691234b7be28d409
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-12-31 21:42:31 +0100

    Add subtotal section, checkout button, continue shopping button to the cart component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Helmet from '../components/Helmet/Helmet';
 import CommonSection from '../components/UI/CommonSection';
 import { Container, Row, Col } from 'reactstrap';
@@ -9,6 +10,7 @@ import SingleCartRow from './SingleCartRow';
 const Cart = () => {
 
   const cartProducts = useSelector((state) => state.cart.cartProducts)
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -47,7 +49,21 @@ const Cart = () => {
                 )
               }
             </Col>
-            <Col lg='3'></Col>
+            <Col lg='3'>
+              <div className={`d-flex align-items-center justify-content-between`}>
+                <h6>Subtotal</h6>
+                <span className={`fs-4 fw-bold`}>${totalAmount}</span>
+              </div>
+              <p className={`fs-6 mt-2`}>taxes and shipping will be calculated in checkout</p>
+              <div>
+                <button className={`${styles.buyBtn} w-100`}>
+                  <Link to="/checkout">Checkout</Link>
+                </button>
+                <button className={`${styles.buyBtn} w-100 mt-3`}>
+                  <Link to="/shop">Continue Shopping</Link>
+                </button>
+              </div>
+            </Col>
           </Row>
         </Container>
       </section>
