ca0f439ce8a7effa0d7eee5ddfabe2006e3724a3
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-01-01 21:50:33 +0100

    Create login and signup form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Helmet from '../components/Helmet/Helmet';
+import { Container, Row, Col, Form, FormGroup } from 'reactstrap';
+import styles from '../styles/login.module.css';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <>
-      <h3>Login</h3>
-    </>
+    <Helmet title='Login'>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6' className={`m-auto text-center`}>
+              <h3 className='fw-bold mb-4'>Login</h3>
+
+              <Form className={styles.authForm}>
+                <FormGroup className={styles.formGroup}>
+                  <input
+                    type="email"
+                    placeholder='Enter your email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className={styles.formGroup}>
+                  <input 
+                    type="password" 
+                    placeholder='Enter your password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+
+                <button
+                  type='submit'
+                  className={`${styles.buyBtn} ${styles.authBtn}`}
+                >
+                  Login
+                </button>
+                <p>Don't have an account <Link to='/signup'>Create an account</Link> </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Helmet from '../components/Helmet/Helmet';
+import { Container, Row, Col, Form, FormGroup } from 'reactstrap';
+import styles from '../styles/login.module.css';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [file, setFile] = useState(null);
+
   return (
-    <>
-      <h3>Signup</h3>
-    </>
+    <Helmet title='Signup'>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6' className={`m-auto text-center`}>
+              <h3 className='fw-bold mb-4'>Signup</h3>
+
+              <Form className={styles.authForm}>
+                <FormGroup className={styles.formGroup}>
+                  <input
+                    type="text"
+                    placeholder='Username'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className={styles.formGroup}>
+                  <input
+                    type="email"
+                    placeholder='Enter your email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className={styles.formGroup}>
+                  <input 
+                    type="password" 
+                    placeholder='Enter your password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className={styles.formGroup}>
+                  <input 
+                    type="file" 
+                    onChange={(e) => setFile(e.target.files[0])}
+                  />
+                </FormGroup>
+
+                <button
+                  type='submit'
+                  className={`${styles.buyBtn} ${styles.authBtn}`}
+                >
+                  Sign up
+                </button>
+                <p>Already have an account <Link to='/login'>Login</Link> </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 
