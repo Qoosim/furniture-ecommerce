30b409016eb67341a86be00a7f70396345dda742
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-01-10 19:58:21 +0100

    Implement logic to post products to the firestore db

--- src/admin/AddProducts.js
+++ src/admin/AddProducts.js
@@ -1,37 +1,149 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Form, FormGroup } from 'reactstrap';
 import styles from './css/addProducts.module.css';
+import { toast } from 'react-toastify';
+import { db, storage } from '../firebase.config';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { collection, addDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 
 const AddProducts = () => {
+
+  const [enterTitle, setEnterTitle] = useState('');
+  const [enterShortDesc, setEnterShortDesc] = useState('');
+  const [enterDescription, setEnterDescription] = useState('');
+  const [enterCategory, setEnterCategory] = useState('');
+  const [enterPrice, setEnterPrice] = useState('');
+  const [enterProductImg, setEnterProductImg] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const addProduct = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    /*
+    const product = {
+      title: enterTitle,
+      shortDesc: enterShortDesc,
+      description: enterDescription,
+      category: enterCategory,
+      price: enterPrice,
+      imgUrl: enterProductImg
+    }
+    */
+    // ===== Add product to the firebase database ====
+
+    try {
+      const docRef = await collection(db, 'product');
+      const storageRef = ref(storage, `productImages/${Date.now() + enterProductImg.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, enterProductImg);
+
+      uploadTask.on(() => {
+        toast.error('Image not uploaded');
+      }, () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+          await addDoc(docRef, {
+            title: enterTitle,
+            shortDesc: enterShortDesc,
+            description: enterDescription,
+            category: enterCategory,
+            price: enterPrice,
+            imgUrl: downloadURL
+          })
+        })
+      })
+      setLoading(false);
+      toast.success('Product successfully added!');
+      navigate('/dashboard/all-products');
+    } catch (err) {
+      setLoading(false);
+      toast.error('Product not added!');
+    }
+  }
+
   return (
     <section>
       <Container>
         <Row>
           <Col lg='12'>
-            <h4>Add Product</h4>
-            <Form>
-              <FormGroup className={styles.formGroup}>
-                <span>Product title</span>
-                <input type="text" placeholder="Title" /> 
-              </FormGroup>
-              <FormGroup className={styles.formGroup}>
-                <span>Short Description</span>
-                <input type="text" placeholder="Lorem..." /> 
-              </FormGroup>
-              <FormGroup className={styles.formGroup}>
-                <span>Description</span>
-                <input type="text" placeholder="Description..." /> 
-              </FormGroup>
-              <FormGroup className={styles.formGroup}>
-                <span>Price</span>
-                <input type="text" placeholder="$100" /> 
-              </FormGroup>
-              <FormGroup className={styles.formGroup}>
-                <span>Category</span>
-                <input type="text" placeholder="Title" /> 
-              </FormGroup>
+            {loading ? (
+              <h4 className="py-5">Loading...</h4>
+            ) : (
+              <>
+                <h4 className="mb-5">Add Product</h4>
+                <Form onSubmit={addProduct}>
+                  <FormGroup className={styles.formGroup}>
+                    <span>Product title</span>
+                    <input
+                      type="text"
+                      placeholder="Title"
+                      value={enterTitle}
+                      onChange={(e) => setEnterTitle(e.target.value)}
+                      required
+                    /> 
+                  </FormGroup>
+                  <FormGroup className={styles.formGroup}>
+                    <span>Short Description</span>
+                    <input
+                      type="text"
+                      placeholder="Short Desc..."
+                      value={enterShortDesc}
+                      onChange={(e) => setEnterShortDesc(e.target.value)}
+                      required
+                    /> 
+                  </FormGroup>
+                  <FormGroup className={styles.formGroup}>
+                    <span>Description</span>
+                    <input
+                      type="text"
+                      placeholder="Description..."
+                      value={enterDescription}
+                      onChange={(e) => setEnterDescription(e.target.value)}
+                      required
+                    /> 
+                  </FormGroup>
+                  <div className="d-flex align-items-center justify-content-between gap-5">
+                    <FormGroup className={`${styles.formGroup} w-50`}>
+                      <span>Price</span>
+                      <input
+                        type="number"
+                        placeholder="$100"
+                        value={enterPrice}
+                        onChange={(e) => setEnterPrice(e.target.value)}
+                        required
+                      /> 
+                    </FormGroup>
+                    <FormGroup className={`${styles.formGroup} w-50`}>
+                      <span>Category</span>
+                      <select
+                        className="w-100"
+                        value={enterCategory}
+                        onChange={(e) => setEnterCategory(e.target.value)}
+                      >
+                        <option value="chair">Chair</option>
+                        <option value="sofa">Sofa</option>
+                        <option value="mobile">Mobile</option>
+                        <option value="watch">Watch</option>
+                        <option value="wireless">Wireless</option>
+                      </select>
+                    </FormGroup>
+                  </div>
 
-            </Form>
+                  <div>
+                    <FormGroup className={styles.formGroup}>
+                      <span>Product Image</span>
+                      <input
+                        type="file"
+                        onChange={(e) => setEnterProductImg(e.target.files[0])}
+                        required
+                      /> 
+                    </FormGroup>
+                  </div>
+                  <button type="submit" className={styles.buyBtn}>Add Product</button>
+                </Form>
+              </>
+            )}
           </Col>
         </Row>
       </Container>
--- src/admin/AllProducts.js
+++ src/admin/AllProducts.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import styles from './css/allProduct.module.css';
+import { Container, Row, Col } from 'reactstrap';
+import useGetData from '../custom-hooks/useGetData';
 
 const AllProducts = () => {
+
+  const { data: productsData } = useGetData('products');
+
   return (
-    <>
-      <h2>All products</h2>
-    </>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12'>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>Title</th>
+                  <th>Category</th>
+                  <th>Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  productsData.map((item) => (
+                   <tr className={styles.productRow} key={item.id}>
+                    <td><img src={item.imgUrl} alt={item.title} /></td>
+                    <td>{item.title}</td>
+                    <td>{item.category}</td>
+                    <td>${item.price}</td>
+                    <td><button className="btn btn-danger">Delete</button></td>
+                  </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
